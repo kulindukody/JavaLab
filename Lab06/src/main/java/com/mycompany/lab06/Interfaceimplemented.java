57b405470b4a8a5a307e7208d0adab12c60d3ad8
Kulindu Kodithuwakku <kulindukodithuwakku@gmail.com>
2023-07-31 04:35:23 -0700

    Add files via upload

---
+++ Lab06/src/main/java/com/mycompany/lab06/Interfaceimplemented.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Kulindu Kodithuwakku
+ */
+package com.mycompany.lab06;
+
+public class Interfaceimplemented implements MyFirstinterface
+{
+    public void display()
+    {
+        x = 10;
+        System.out.println(x);
+    }
+    //
+}
---
+++ Lab06/src/main/java/com/mycompany/lab06/Lab06.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Kulindu Kodithuwakku
+ */
+package com.mycompany.lab06;
+
+public class Lab06 {
+
+    public static void main(String[] args) 
+    {
+        
+    }
+}
